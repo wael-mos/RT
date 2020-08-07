d65563e71d2996e1480ec5af6d49755694370544
Walid <welmossannif@gmail.com>
2020-08-07 12:01:41 +0200

    check double precision

--- incs/rt.h
+++ incs/rt.h
@@ -26,6 +26,14 @@
 # include <libxml/encoding.h>
 # include <libxml/xmlwriter.h>
 
+#ifdef cl_khr_fp64
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+#elif defined(cl_amd_fp64)
+#pragma OPENCL EXTENSION cl_amd_fp64 : enable
+#else
+#error "Double precision floating point not supported by OpenCL implementation."
+#endif
+
 # define CL_SILENCE_DEPRECATION
 # ifdef __APPLE__
 #  include <OpenCL/cl.h>
